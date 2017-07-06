c9fce07eb76d2c85d1d7ee4806b47daddc3d7a95
baoxina <bx19920704@sina.com>
2017-07-06 16:29:12 +0800

    补上条
    Signed-off-by: baoxina <bx19920704@sina.com>

--- src/com/baoxina/annotation/BaoxinaAnnotation.java
+++ src/com/baoxina/annotation/BaoxinaAnnotation.java
@@ -1,7 +1,9 @@
 package com.baoxina.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  *  元数据
@@ -73,6 +75,7 @@ import java.lang.annotation.RetentionPolicy;
 	public   @interface  SuppressWarnings 
  *
  */
+@Target({ElementType.TYPE,ElementType.METHOD})//元注解
 @Retention(RetentionPolicy.RUNTIME)//元注解
 public @interface BaoxinaAnnotation {
 	String color() default "red";
